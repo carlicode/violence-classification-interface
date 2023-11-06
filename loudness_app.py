67cd0a80ad90eed44fb9eccf1aba46a6ec981898
Carla Florida <carli.f.roman@gmail.com>
2023-11-06 11:56:21 -0400

    loudness app

--- loudness_app.py
+++ loudness_app.py
@@ -1,28 +1,114 @@
 import streamlit as st
-from pydub import AudioSegment
+import tensorflow as tf
 import numpy as np
+import librosa
+from sklearn.preprocessing import LabelEncoder
+import soundfile as sf
+import pandas as pd
+import io
 
-# Define la función para calcular el loudness por segundo
-def calculate_loudness_per_second(audio_file):
-    try:
-        audio = AudioSegment.from_file(audio_file)
-        duration_in_seconds = len(audio) / 1000
-        loudness_values = []
 
-        for second in range(int(duration_in_seconds)):
-            start_time = second * 1000  # Milisegundos
-            end_time = (second + 1) * 1000  # Milisegundos
-            segment = audio[start_time:end_time]
-            loudness = segment.dBFS
-            loudness_values.append(loudness)
+from calculate_loudness import calculate_loudness_per_second
 
-        return loudness_values
-    except Exception as e:
-        return None
+model = tf.keras.models.load_model('experimento.h5')
+labels = ["crying", "glass_breaking", "screams", "gun_shot", "people_talking"]
 
-# Resto de tu código Streamlit
+label_encoder = LabelEncoder()
+label_encoder.fit(labels)  
 
 st.title('Detector de Audio')
+
+uploaded_file = st.file_uploader("Sube un archivo de audio largo", type=["wav"])
+
+def hacer_prediccion(audio_data):
+    audio, _ = librosa.load(audio_data, sr=16000)
+
+    segment_duration = 1  # Duración
+    samples_per_segment = int(16000 * segment_duration)
+    num_segments = len(audio) // samples_per_segment
+
+    predictions = []
+
+    for i in range(num_segments):
+        start_sample = i * samples_per_segment
+        end_sample = (i + 1) * samples_per_segment
+        segment = audio[start_sample:end_sample]
+
+        # Espectrograma
+        spectrogram = librosa.feature.melspectrogram(y=segment, sr=16000)
+        input_data = np.expand_dims(spectrogram, axis=0)
+
+        prediction = model.predict(input_data)[0]
+
+        decoded_labels = label_encoder.inverse_transform(range(len(labels)))
+        
+        segment_predictions = []
+        for label, conf in zip(decoded_labels, prediction):
+            if conf >= 0.75:
+                segment_predictions.append((label, conf * 100))
+        
+        if not segment_predictions:
+            segment_predictions.append(("No se identificaron etiquetas", 0))
+        
+        predictions.append(segment_predictions)
+
+    return predictions
+
+if uploaded_file:
+    st.write("Procesando el archivo de audio largo...")
+    st.audio(uploaded_file, format="audio/wav")
+    predictions_per_second = hacer_prediccion(uploaded_file)
+
+    st.write("Predicciones por segundo:")
+    for i, segment_predictions in enumerate(predictions_per_second):
+        st.write(f"Segundo {i + 1}:")
+        for label, confidence in segment_predictions:
+            st.write(f"  Etiqueta: {label}, Confianza: {confidence:.2f}%")
+
+
+model = tf.keras.models.load_model('experimento.h5')
+labels = ["crying", "glass_breaking", "screams", "gun_shot", "people_talking"]
+
+label_encoder = LabelEncoder()
+label_encoder.fit(labels)  
+
+st.title('Detector de Audio')
+
+uploaded_file = st.file_uploader("Sube un archivo de audio largo", type=["wav"])
+
+def hacer_prediccion(audio_data):
+    audio, _ = librosa.load(audio_data, sr=16000)
+
+    segment_duration = 1  # Duración
+    samples_per_segment = int(16000 * segment_duration)
+    num_segments = len(audio) // samples_per_segment
+
+    predictions = []
+
+    for i in range(num_segments):
+        start_sample = i * samples_per_segment
+        end_sample = (i + 1) * samples_per_segment
+        segment = audio[start_sample:end_sample]
+
+        # Espectrograma
+        spectrogram = librosa.feature.melspectrogram(y=segment, sr=16000)
+        input_data = np.expand_dims(spectrogram, axis=0)
+
+        prediction = model.predict(input_data)[0]
+
+        decoded_labels = label_encoder.inverse_transform(range(len(labels)))
+        
+        segment_predictions = []
+        for label, conf in zip(decoded_labels, prediction):
+            if conf >= 0.75:
+                segment_predictions.append((label, conf * 100))
+        
+        if not segment_predictions:
+            segment_predictions.append(("No se identificaron etiquetas", 0))
+        
+        predictions.append(segment_predictions)
+
+    return predictions
 
 uploaded_file = st.file_uploader("Sube un archivo de audio largo", type=["wav"])
 
