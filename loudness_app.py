4bda87cb8b20e588509da66ee21d47f6d8d19de3
Carla Florida <carli.f.roman@gmail.com>
2023-11-06 18:26:53 -0400

    loudness app

--- loudness_app.py
+++ loudness_app.py
@@ -1,14 +1,18 @@
-import pydub
+import streamlit as st
 import tensorflow as tf
 import numpy as np
 import librosa
 from sklearn.preprocessing import LabelEncoder
 import soundfile as sf
 import pandas as pd
-import streamlit as st
+from pydub import AudioSegment
+from io import BytesIO
+import matplotlib.pyplot as plt
 
 model = tf.keras.models.load_model('experimento.h5')
 labels = ["crying", "glass_breaking", "screams", "gun_shot", "people_talking"]
+
+loudness_values = []  # Almacenar los valores de loudness a lo largo del tiempo
 
 label_encoder = LabelEncoder()
 label_encoder.fit(labels)
@@ -18,12 +22,12 @@ st.title('Detector de Audio')
 uploaded_file = st.file_uploader("Subir un archivo de audio largo", type=["wav"])
 
 def calcular_loudness(segment):
-    audio = pydub.AudioSegment(
-        segment.tobytes(),
-        frame_rate=16000,
-        sample_width=segment.sample_width,
-        channels=segment.channels
-    )
+    buffer = BytesIO()
+    sf.write(buffer, segment, 16000, subtype='PCM_16', format='wav')
+    buffer.seek(0)
+
+    audio = AudioSegment.from_wav(buffer)
+
     loudness = audio.rms
     return loudness
 
@@ -42,6 +46,7 @@ def hacer_prediccion(audio_data):
         segment = audio[start_sample:end_sample]
 
         loudness = calcular_loudness(segment)
+        loudness_values.append(loudness)
 
         spectrogram = librosa.feature.melspectrogram(y=segment, sr=16000)
         input_data = np.expand_dims(spectrogram, axis=0)
@@ -60,7 +65,16 @@ def hacer_prediccion(audio_data):
 
         data.extend(segment_predictions)
 
+
     return data
+
+def crear_grafico_loudness(loudness_values):
+    # Crear un gráfico de loudness a lo largo del tiempo
+    plt.figure(figsize=(10, 5))
+    plt.plot(range(len(loudness_values)), loudness_values)
+    plt.xlabel("Tiempo (segmentos)")
+    plt.ylabel("Loudness")
+    st.pyplot(plt)
 
 if uploaded_file:
     st.write("Procesando el archivo de audio largo...")
@@ -70,3 +84,4 @@ if uploaded_file:
     st.write("Predicciones por segundo:")
     predictions_df = pd.DataFrame(predictions_per_second, columns=["Segundo", "Etiqueta", "Confianza", "Loudness"])
     st.table(predictions_df)
+    crear_grafico_loudness(loudness_values)
