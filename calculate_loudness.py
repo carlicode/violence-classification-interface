2233b568bf5c0f3106581a51d6aaf4fe72ef4c4c
Carla Florida <carli.f.roman@gmail.com>
2023-11-06 11:58:31 -0400

    loudness app

--- calculate_loudness.py
+++ calculate_loudness.py
@@ -1,18 +1,19 @@
-from pydub import AudioSegment
-import numpy as np
+import librosa
 
 def calculate_loudness_per_second(audio_file):
     try:
-        audio = AudioSegment.from_file(audio_file)
-        duration_in_seconds = len(audio) / 1000
+        audio, _ = librosa.load(audio_file, sr=16000)
+        duration_in_seconds = len(audio) / 16000  # Duración en segundos
         loudness_values = []
 
         for second in range(int(duration_in_seconds)):
-            start_time = second * 1000  # Milisegundos
-            end_time = (second + 1) * 1000  # Milisegundos
-            segment = audio[start_time:end_time]
-            loudness = segment.dBFS
-            loudness_values.append(loudness)
+            start_sample = second * 16000  # Muestras por segundo (16000 muestras por segundo)
+            end_sample = (second + 1) * 16000  # Muestras por segundo
+            segment = audio[start_sample:end_sample]
+
+            # Calcular el loudness utilizando el valor RMS (Root Mean Square) de la señal
+            loudness = librosa.feature.rms(segment)
+            loudness_values.append(loudness[0][0])  # Tomar el valor RMS como loudness
 
         return loudness_values
     except Exception as e:
